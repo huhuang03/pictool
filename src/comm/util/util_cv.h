f6e251110f62abacc054c1168915e8869b090801
huwenfan <huhuang03@gmail.com>
2022-01-19 18:55:23 +0800

    up

--- src/comm/util/util_cv.h
+++ src/comm/util/util_cv.h
@@ -6,7 +6,9 @@
 #define PICTOOL_SRC_UTIL_UTIL_CV_H_
 #include <QImage>
 #include <opencv2/core.hpp>
+#include <string>
 
 bool QPointInMat(const cv::Mat &img, QPoint point);
 void QImage2Mat(const QImage &src, cv::OutputArray out);
+
 #endif //PICTOOL_SRC_UTIL_UTIL_CV_H_
